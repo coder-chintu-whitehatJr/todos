85e06eae0dc77b38af366a16dc851e16962a3b0f
coder-chintu-whitehatJr <77052831+coder-chintu-whitehatJr@users.noreply.github.com>
2021-02-04 15:33:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Todo</title>
+</head>
+<body>
+    <h1><?php echo "This is a test page"; ?></h1>
+</body>
+</html>
